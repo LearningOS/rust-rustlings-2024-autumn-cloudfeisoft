3c3164d6b980c1c13e7ffa55476a212b63498f67
cloudfeisoft <cloudfeisoft@qq.com>
2024-10-08 13:02:17 +0800

    strings done

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,19 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
-fn compose_me(input: &str) -> String {
+fn compose_me(input: String) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!",input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
@@ -33,8 +31,8 @@ mod tests {
 
     #[test]
     fn compose_a_string() {
-        assert_eq!(compose_me("Hello"), "Hello world!");
-        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+        assert_eq!(compose_me("Hello".to_string()), "Hello world!");
+        assert_eq!(compose_me("Goodbye".to_string()), "Goodbye world!");
     }
 
     #[test]
